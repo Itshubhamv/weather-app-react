ac4657d850d85fd5f0905420e971f08c8c38ee9f
Shubham Verma <86148861+Itshubhamv@users.noreply.github.com>
2023-05-20 22:49:27 +0530

    Add files via upload

---
+++ -weather-app-react-main/src/Credits.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Credits.css"
+
+export default function Credits() {
+    return (
+      <div className="Credits">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h6 className="credits-section">
+                {" "}
+                Coded by shubham verma as a assignment for glaztower ,{" "}
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
